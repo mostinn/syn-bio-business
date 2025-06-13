9875391384188ed1dc9f361511e6c9b27f98faa3
mostinn <mostinn@gmail.com>
2025-06-13 14:52:27 -0400

    Add SynBio Business Quarto slides

---
+++ Git-SynBio-Business.bash
@@ -0,0 +1,8 @@
+git init
+git add .
+git commit -m "Add SynBio Business Quarto slides"
+git branch -M main
+git remote add origin https://github.com/mostinn/syn-bio-business.git
+git push -u origin main
+
+
